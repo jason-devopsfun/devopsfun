c35b5d51dcfacfe4f440c34cc2f6fefee0304636
thejasonkendall <thejasonkendall@gmail.com>
2025-04-07 09:04:43 -0400

    test4

--- demo-api/src/main/java/com/example/demo_api/HelloController.java
+++ demo-api/src/main/java/com/example/demo_api/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Java API in Docker! TEST 3";
+        return "Hello from Java API in Docker! TEST 4";
     }
 }
